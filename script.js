77fd51202c7aae55ca3ed47554585c0e0c447647
Oscar Jimenez <o4jimene@ucsd.edu>
2022-04-25 23:01:17 -0700

    code clean + image resize

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
 
 /* --------------------- Header Popout CSS ---------------------------------*/
+/*
 const banner = document.querySelector(".banner"); 
 
 const pageHeader = document.querySelector("header");
-
-// large page
 
 var options = {
 };
 
 const bannerObserver = new IntersectionObserver(function(entries, mainObserver){
-   entries.forEach(entry =>{
-       console.log(options);
-       if(!entry.isIntersecting) {
-           pageHeader.classList.add("popout-header");
-       } else{
-           pageHeader.classList.remove('popout-header');
-       }
-   }) ;
+    entries.forEach(entry =>{
+        console.log(options);
+        if(!entry.isIntersecting) {
+            pageHeader.classList.add("popout-header");
+        } else{
+            pageHeader.classList.remove('popout-header');
+        }
+    }) ;
 }, options);
 
 
 bannerObserver.observe(banner);
+*/
 
 /* --------------------------------------------------------------------------*/
 
