bb22e3c8085d5c9a5664dad6fe57a4f350bd43dc
Oscar Jimenez <o4jimene@ucsd.edu>
2022-05-23 15:52:15 -0700

    base

--- src/js/App.js
+++ src/js/App.js
@@ -1,15 +1,207 @@
 import React from "react";
 import { createRoot } from "react-dom";
 
-const sectionList = [
-  "Jumpers",
-  "Tables & Chairs",
-  "Tents & Canopies",
-  "Other Items",
+const data = [
+  {
+    category: "jumpers",
+    items: [
+      {
+        imageUrl: "/images/jumper_blue_11.jpg",
+        itemInfo: {
+          itemName: "Blue Castle #1",
+          itemDescription: "111ft W x 11ft L ",
+          itemCost: "$70",
+        },
+      },
+      {
+        imageUrl: "/images/jumper_blue_13.jpeg",
+        itemInfo: {
+          itemName: "Blue Castle #2",
+          itemDescription: "13ft W x 13ft L",
+          itemCost: "$70",
+        },
+      },
+      {
+        imageUrl: "./images/jumper_pink_11",
+        itemInfo: {
+          itemName: "Pink Castle #1",
+          itemDescription: "11ft W x 11ft L",
+          itemCost: "$70",
+        },
+      },
+      {
+        imageUrl: "./images/jumper_pink_13.jpeg",
+        itemInfo: {
+          itemName: "Pink Castle #2",
+          itemDescription: "13ft W x 13ft L ",
+          itemCost: "$70",
+        },
+      },
+      {
+        imageUrl: "./images/jumper_combo.jpeg",
+        itemInfo: {
+          itemName: "Combo Slide",
+          itemDescription: "16ft W x 24ft L ",
+          itemCost: "$110",
+        },
+      },
+      {
+        imageUrl: "./images/jumper_jungle.jpeg",
+        itemInfo: {
+          itemName: "Jungle",
+          itemDescription: "13ft W x 13ft L ",
+          itemCost: "$70",
+        },
+      },
+      {
+        imageUrl: "./images/jumper_sport.jpeg",
+        itemInfo: {
+          itemName: "Sports",
+          itemDescription: "13ft W x 13ft L ",
+          itemCost: "$70",
+        },
+      },
+      {
+        imageUrl: "./images/waterslide.jpeg",
+        itemInfo: {
+          itemName: "Waterslide",
+          itemDescription: "10ft H x 13ft W x 22ft L",
+          itemCost: "$160",
+        },
+      },
+    ],
+  },
+  {
+    category: "Tables & Chairs",
+    items: [
+      {
+        imageUrl: "./images/table_rectangle_6.jpeg",
+        itemInfo: {
+          itemName: "Rectangular Tables",
+          itemDescription: "",
+          itemCost: "$5",
+        },
+      },
+      {
+        imageUrl: "./images/table_round_60.jpeg",
+        itemInfo: {
+          itemName: "Large Round Table",
+          itemDescription: "",
+          itemCost: "$8",
+        },
+      },
+      {
+        imageUrl: "./images/table_round_48.jpeg",
+        itemInfo: {
+          itemName: "Small Round Table",
+          itemDescription: "",
+          itemCost: "$5",
+        },
+      },
+      {
+        imageUrl: "./images/table_chair_kids.jpeg",
+        itemInfo: {
+          itemName: "Kids Chairs & Tables",
+          itemDescription: "Tables $5.00 each and Chairs $0.50 each ",
+          itemCost: "",
+        },
+      },
+      {
+        imageUrl: "./images/table_cocktail.jpeg",
+        itemInfo: {
+          itemName: "Cocktail Table",
+          itemDescription: "2.5ft or 3.5ft tall",
+          itemCost: "$8",
+        },
+      },
+      {
+        imageUrl: "./images/chair_white.jpeg",
+        itemInfo: {
+          itemName: "White Folding Chair",
+          itemDescription: "",
+          itemCost: "$1",
+        },
+      },
+      {
+        imageUrl: "./images/chair_beige.jpeg",
+        itemInfo: {
+          itemName: "Beige Folding Chair",
+          itemDescription: "",
+          itemCost: "$0.75",
+        },
+      },
+    ],
+  },
+  {
+    category: "Tents & Canopies",
+    items: [
+      {
+        imageUrl: "./images/tent_12x20.jpeg",
+        itemInfo: {
+          itemName: "12ft x 20ft Tent",
+          itemDescription: "",
+          itemCost: "$100",
+        },
+      },
+      {
+        imageUrl: "./images/tent_12X30.jpeg",
+        itemInfo: {
+          itemName: "12ft x 30ft Tent",
+          itemDescription: "",
+          itemCost: "$125",
+        },
+      },
+      {
+        imageUrl: "./images/tent_20x30.jpeg",
+        itemInfo: {
+          itemName: "20ft x 30ft Tent",
+          itemDescription: "",
+          itemCost: "$150",
+        },
+      },
+      {
+        imageUrl: "./images/canopy.jpeg",
+        itemInfo: {
+          itemName: "13ft x 13ft Canopy",
+          itemDescription: "",
+          itemCost: "$35",
+        },
+      },
+    ],
+  },
+  {
+    category: "Other Items",
+    items: [
+      {
+        imageUrl: "./images/patio_heater.jpeg",
+        itemInfo: {
+          itemName: "Patio Heater",
+          itemDescription: "",
+          itemCost: "$65",
+        },
+      },
+      {
+        imageUrl: "./images/snowcone_machine.jpeg",
+        itemInfo: {
+          itemName: "Snowcone Machine",
+          itemDescription: "",
+          itemCost: "$50",
+        },
+      },
+      {
+        imageUrl: "./images/portable_bathroom.jpeg",
+        itemInfo: {
+          itemName: "Portable Bathroom",
+          itemDescription: "",
+          itemCost: "$100",
+        },
+      },
+    ],
+  },
 ];
 
-const ItemImage = () => {
-  return <img className="item-image" src="/images/jumper_blue_11.jpeg" />;
+const ItemImage = (props) => {
+  return <img className="item-image" src={props.imageUrl} alt="" />;
 };
 
 const ItemCaption = (props) => {
@@ -22,14 +214,16 @@ const ItemCaption = (props) => {
   );
 };
 
-const Item = () => {
+const Item = (props) => {
   return (
     <figure className="item">
-      <ItemImage />
+      <ItemImage key={props.imageUrl} imageUrl={props.imgUrl} />
+
       <ItemCaption
-        itemName="Blue Castle #1"
-        itemDescription="11ft W x 11ft L"
-        itemCost="$70"
+        key={props.itemInfo.itemName}
+        itemName={props.itemInfo.itemName}
+        itemDescription={props.itemInfo.itemDescription}
+        itemCost={props.itemInfo.itemCost}
       />
     </figure>
   );
@@ -39,9 +233,17 @@ const ItemSection = (props) => {
   return (
     <div className="item-section">
       <h2 className="section-header">{props.sectionName}</h2>
-      <section className="grid-container" tabIndex="0">
+      <section className="grid-container">
         <div className="item-grid">
-          <Item />
+          {props.sectionItems.map((item) => {
+            return (
+              <Item
+                key={item.imageUrl}
+                imgUrl={item.imageUrl}
+                itemInfo={item.itemInfo}
+              />
+            );
+          })}
         </div>
       </section>
     </div>
@@ -51,7 +253,15 @@ const ItemSection = (props) => {
 const Catalog = () => {
   return (
     <div id="catalog">
-      <ItemSection sectionName="Test" />
+      {data.map((obj) => {
+        return (
+          <ItemSection
+            key={obj.category}
+            sectionName={obj.category}
+            sectionItems={obj.items}
+          />
+        );
+      })}
     </div>
   );
 };
